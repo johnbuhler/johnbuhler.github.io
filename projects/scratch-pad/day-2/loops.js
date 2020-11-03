3f8ab8827c0c07f6c6d755ec04da6ce5e52c0cd6
Ubuntu <ubuntu@ip-172-31-31-216.ec2.internal>
2020-11-03 21:44:45 +0000

    add portfolio.html file

--- projects/scratch-pad/day-2/loops.js
+++ projects/scratch-pad/day-2/loops.js
@@ -12,7 +12,10 @@
  */
 function printArrayValues(array) {
   // YOUR CODE BELOW HERE //
-  
+
+for (var i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
   
   
   
@@ -26,7 +29,9 @@ function printArrayValues(array) {
 function printArrayValuesInReverse(array) {
   // YOUR CODE BELOW HERE //
   
-  
+for (var i = array.length - 1; i >= 0; i--) {
+  console.log(array[i]);
+}
   
   
   // YOUR CODE ABOVE HERE //
@@ -38,8 +43,7 @@ function printArrayValuesInReverse(array) {
 function getObjectKeys(object) {
   // YOUR CODE BELOW HERE //
   
-  
-  
+return Object.keys(object);
   
   // YOUR CODE ABOVE HERE //
 }
@@ -51,7 +55,9 @@ function getObjectKeys(object) {
 function printObjectKeys(object) {
   // YOUR CODE BELOW HERE //
   
-  
+for (var key in object) {
+  console.log(key);
+}  
   
   
   // YOUR CODE ABOVE HERE //
@@ -63,7 +69,7 @@ function printObjectKeys(object) {
 function getObjectValues(object) {
   // YOUR CODE BELOW HERE //
   
-  
+return Object.values(object);
   
   
   // YOUR CODE ABOVE HERE //
@@ -76,7 +82,9 @@ function getObjectValues(object) {
 function printObjectValues(object) {
   // YOUR CODE BELOW HERE //
   
-  
+for (var value in object) {
+  console.log(object[value]);
+}  
   
   
   // YOUR CODE ABOVE HERE //
@@ -88,7 +96,7 @@ function printObjectValues(object) {
 function getObjectLength(object) {
   // YOUR CODE BELOW HERE //
   
-  
+return Object.keys(object).length; 
   
   
   // YOUR CODE ABOVE HERE //
@@ -101,6 +109,17 @@ function getObjectLength(object) {
 function printObjectValuesInReverse(object) {
   // YOUR CODE BELOW HERE //
   
+  //assign var arr to an empty array
+  var arr = [];
+  //loops over object keys and pushes them into the empty array
+  for (var key in object) {
+    arr.push(object[key]);
+  }
+  
+  //loops over the array in reverse and prints the values
+  for (var i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+  }
   
   
   
